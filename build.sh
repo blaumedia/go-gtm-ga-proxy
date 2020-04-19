2a097c648d26b9cad5e4a581a3e549e964eca0a4
Dennis Paul <dennis@blaumedia.com>
2020-04-19 02:26:24 +0200

    [*] Typo in build.sh script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker build \
     --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
     --build-arg VCS_REF=$(git log --pretty=format:'%h' -n 1) \
     --build-arg BUILD_VERSION="1.0.0" \
-    -t blaumedia/gogtmgaproxy:latest \
+    -t blaumedia/go-gtm-ga-proxy:latest \
     .
 
 docker push blaumedia/go-gtm-ga-proxy:latest
